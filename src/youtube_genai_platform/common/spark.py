2290e3ffb5c523caac8b27f782aa7c5bb46b739d
Airton Carneiro <airtoncarneiro@gmail.com>
2026-06-08 21:15:52 -0300

    Refactor Spark integration and remove unused files; add new setup notebook

--- src/youtube_genai_platform/common/spark.py
+++ src/youtube_genai_platform/common/spark.py
@@ -1,5 +1,26 @@
 from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame
 
 
+def get_taxis(spark: SparkSession) -> DataFrame:
+    return spark.read.table("samples.nyctaxi.trips")
+
+# def get_spark() -> SparkSession:
+#     return SparkSession.getActiveSession()
+
+# Create a new Databricks Connect session. If this fails,
+# check that you have configured Databricks Connect correctly.
+# See https://docs.databricks.com/dev-tools/databricks-connect.html.
 def get_spark() -> SparkSession:
-    return SparkSession.getActiveSession()
+    try:
+        from databricks.connect import DatabricksSession
+        return DatabricksSession.builder.getOrCreate()
+    except ImportError:
+        return SparkSession.builder.getOrCreate()
+
+def main():
+    get_taxis(get_spark()).show(5)
+
+
+if __name__ == "__main__":
+    main()
